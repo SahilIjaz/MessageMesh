6fb85e3add310e4ec6e817b470ba764a0e2d9cab
SahilIjaz <hssahil2913@gmail.com>
2026-04-21 19:57:20 +0500

    fixes

--- services/group-chat-service/knexfile.js
+++ services/group-chat-service/knexfile.js
@@ -1,7 +1,7 @@
 module.exports = {
   development: {
     client: 'pg',
-    connection: {
+    connection: process.env.DATABASE_URL || {
       host: process.env.DATABASE_HOST || 'postgres',
       port: process.env.DATABASE_PORT || 5432,
       user: process.env.DATABASE_USER || 'messagemesh',
